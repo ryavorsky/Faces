33a9fe955c66df82efdd09b9d02601250073fda4
ryavorsky <ryavorsky@gmail.com>
2014-07-06 10:08:34 +0400

    Page B

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // JavaScript source code
 function a() {
-    str = '<h1  style="text-align:center">Найди себя!</h1>';
+    str = '<h1 style="text-align:center">Найди себя!</h1>';
     str += '<table border = "0"  cellpadding = "12" align="center"><tr>';
     for (i=0; i<16 ; i++){
 
-        str += '<td width="100"  align="center" ><div id="block' + i + '">'
+        str += '<td width="100"  align="center" ><div id="block' + i + '" onclick = "b(this.id)">'
         str += '<p> ' + x[i][0] + '\n';
         str += '<p> <img src = "' + x[i][1] + '" width="90">';
         str += '</div></td>';
@@ -15,9 +15,5 @@ function a() {
     }
     str += '</tr></table>';
     page.innerHTML = str;
-}
-
-function b(sendr,val){
-    sender.style.color = val
 }
 
