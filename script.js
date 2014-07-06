cd59058a23db8c2286d6cb156edbdd4363bcd714
ryavorsky <ryavorsky@gmail.com>
2014-07-06 13:10:50 +0400

    Page C

--- script.js
+++ script.js
@@ -2,11 +2,11 @@
 function a() {
     str = '<h1 style="text-align:center">Найди себя!</h1>';
     str += '<table border = "0"  cellpadding = "12" align="center"><tr>';
-    for (i=0; i<16 ; i++){
+    for (i=0; i<x.length ; i++){
 
         str += '<td width="100"  align="center" ><div id="block' + i + '" onclick = "b(this.id)">'
         str += '<p> ' + x[i][0] + '\n';
-        str += '<p> <img src = "' + x[i][1] + '" width="90">';
+        str += '<p> <img src = "pic/' + x[i][1] + '" width="90">';
         str += '</div></td>';
 
         if (i%7 == 6){
